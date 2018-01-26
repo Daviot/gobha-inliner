852de5f016fd19536f6ecf68f008bdef2d5ae96b
patrick@sales-suckers.com <patrick@sales-suckers.com>
2018-01-26 12:02:07 +0100

    cleanup

--- inliner.js
+++ inliner.js
@@ -4,7 +4,7 @@ var inlineSource = require('inline-source').sync;
 var minimatch = require("minimatch");
 
 /*
- * Filter files to find HTML files.
+ * Filter files to find files, that match the minimatch array filter.
  * Iterate through each file name and grab
  * the contents of each file and replace
  * with an inlined version.
@@ -20,22 +20,11 @@ var inline = function(options) {
         options.pattern = [options.pattern];
     }
   return function(files, metalsmith, done) {
-    // var htmlFiles = Object.keys(files).filter(function(file) {
-    //   // Make sure the filename ends in either htm or html
-    // //   var checkIfInline = /[.](?:html?)$/.test(file);
-    //   var checkIfInline = options.filter((o) => {return file.indexOf(o) > -1 && file.lastIndexOf(o) == file.length - o.length}).length > 0;
-    //   console.log(checkIfInline, file)
-
-    //   if (checkIfInline) {
-    //     return file;
-    //   }
-    // });
     var htmlFiles = Object.keys(files).filter((file) => { return options.pattern.filter((pattern) => {
         return minimatch(file, pattern, { matchBase: true })
     }).length > 0});
 
     htmlFiles.map(function(path) {
-    //console.log(path)
       files[path].contents = new Buffer(inlineSource(files[path].contents.toString(), options)), 'utf8';
     });
     done();
