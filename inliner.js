0517009b5ce56a2fd7e1524d7618ca4561d254b0
patrick@sales-suckers.com <patrick@sales-suckers.com>
2018-01-31 10:49:06 +0100

    update dependency inline-source to 6.1.1

--- inliner.js
+++ inliner.js
@@ -1,7 +1,7 @@
-"use strict";
+'use strict';
 
-var inlineSource = require("inline-source").sync;
-var minimatch = require("minimatch");
+var inlineSource = require('inline-source').inlineSource;
+var minimatch = require('minimatch');
 
 /*
  * Filter files to find files, that match the minimatch array filter.
@@ -11,31 +11,38 @@ var minimatch = require("minimatch");
  */
 
 var inline = function(options) {
-  console.log(options);
-  var patternDefault = ["*.html"];
-  if (options.pattern == null) {
-    options.pattern = patternDefault;
-  }
-  if (typeof options.pattern == "String") {
-    options.pattern = [options.pattern];
-  }
-  return function(files, metalsmith, done) {
-    var htmlFiles = Object.keys(files).filter(file => {
-      return (
-        options.pattern.filter(pattern => {
-          return minimatch(file, pattern, { matchBase: true });
-        }).length > 0
-      );
-    });
+    // console.log(options);
+    var patternDefault = ['*.html'];
+    if (options.pattern == null) {
+        options.pattern = patternDefault;
+    }
+    if (typeof options.pattern == 'String') {
+        options.pattern = [options.pattern];
+    }
+    return function(files, metalsmith, done) {
+        var htmlFiles = Object.keys(files).filter(file => {
+            return (
+                options.pattern.filter(pattern => {
+                    return minimatch(file, pattern, { matchBase: true });
+                }).length > 0
+            );
+        });
 
-    htmlFiles.map(function(path) {
-      (files[path].contents = new Buffer(
-        inlineSource(files[path].contents.toString(), options)
-      )),
-        "utf8";
-    });
-    done();
-  };
+        htmlFiles.map(async function(path) {
+            try {
+                var content = files[path].contents.toString();
+                var inlined = await inlineSource(content, {
+                  compress: true
+                });
+                files[path].contents = Buffer.from(inlined, 'utf8');
+            } catch (e) {
+                console.error('[ERROR] inliner error in', path);
+                console.error(e);
+            }
+        });
+
+        done();
+    };
 };
 
 module.exports = inline;
